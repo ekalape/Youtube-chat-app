7546aa4143814b2b92169e08a0c3cc9d0224dcbb
Ekalape <ekalape@outlook.it>
2023-10-24 10:47:12 +0200

    feat: implement color indicators for publication date

--- youtube-app/src/app/components/card/card.component.ts
+++ youtube-app/src/app/components/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IYoutubeItem } from 'src/app/models/youtube-item.model';
+import { TimeDistanceColor } from 'src/app/utils/enums';
 
 @Component({
   selector: 'app-card',
@@ -10,20 +11,34 @@ import { IYoutubeItem } from 'src/app/models/youtube-item.model';
 })
 export class CardComponent implements OnInit {
   id: string | null = '';
-  dateStatus: string = "";
+
+  timeDiff: number = 0;
+  timeDistance: TimeDistanceColor = TimeDistanceColor.new
 
 
 
   @Input() cardData: IYoutubeItem | null = null
 
   constructor(private route: ActivatedRoute) {
+
   }
 
   ngOnInit() {
 
-
     this.id = this.route.snapshot.paramMap.get('itemId');
-    this.dateStatus = "#3F51B5"
-    // console.log('id', this.id);
+    const published = this.cardData?.snippet.publishedAt;
+    if (published) {
+      this.timeDiff = (Date.now() - Date.parse(published)) / 1000
+
+    }
+
+    if (this.timeDiff < 604800) this.timeDistance = TimeDistanceColor.new;
+    else if (this.timeDiff >= 604800 && this.timeDiff < 2592000) this.timeDistance = TimeDistanceColor.medium;
+    else if (this.timeDiff >= 2592000 && this.timeDiff < 15552000) this.timeDistance = TimeDistanceColor.old;
+    else this.timeDistance = TimeDistanceColor.oldiest;
+    console.log(this.timeDiff);
+
+
+
   }
 }
---
+++ youtube-app/src/app/utils/enums.ts
@@ -0,0 +1,6 @@
+export enum TimeDistanceColor {
+  oldiest = "#F8312F",
+  old = "#FFB02E",
+  medium = "#00D26A",
+  new = "#00A6ED",
+}
